16a687a1cc3e4f73465f44b9d04c84cc579d9f4b
Homa <mohammad.masoomihomayoun@theapsgroup.com>
2026-05-25 17:59:28 +0200

    feat: add global styles and layout structure
    - Introduced a new CSS file for global styles including Tailwind CSS setup.
    - Created layout styles for site header, sidebar, content area, and footer.
    - Added build and static manifests for improved routing and static site generation.
    - Implemented hot module replacement for better development experience.

--- .next/static/chunks/app/page-f7ea9caee226182f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[165,974],{9670:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4400,23))}},e=>{e.O(0,[400,933,146,358],()=>e(e.s=9670)),_N_E=e.O()}]);
